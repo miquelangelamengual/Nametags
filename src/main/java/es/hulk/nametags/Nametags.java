096ea9c800ce60bb418fc0fe218f6d98e724123b
Miquel Angel <hulkrojo2@gmail.com>
2022-08-28 03:17:23 +0200

    Initial commit

---
+++ src/main/java/es/hulk/nametags/Nametags.java
@@ -0,0 +1,17 @@
+package es.hulk.nametags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nametags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
